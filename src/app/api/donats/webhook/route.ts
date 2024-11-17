2efaea7c2c8165956e181b1fd9ba6417b6fe6e1b
Andrii Shalankevych <shalankevych@gmail.com>
2024-11-17 19:41:13 +0200

    Add debug logs for webhook

--- src/app/api/donats/webhook/route.ts
+++ src/app/api/donats/webhook/route.ts
@@ -16,10 +16,17 @@ export async function GET(request: NextRequest) {
 }
 
 export async function POST(request: NextRequest) {
+	console.info("Mono just called webhook");
 	const requestHeaders = new Headers(request.headers);
 	requestHeaders.forEach((value, key) => console.info(`HEADER ${key} HAS VALUE ${value}`));
 	const data = await request.json();
 	console.info("Deceived donat data", data);
+	return new Response(JSON.stringify({}), {
+		status: 200,
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
 	// const appAuthToken = process.env.APP_AUTH_TOKEN;
 	// const requestAuthToken = requestHeaders.get("X-App-Auth-Token");
 	// if (appAuthToken !== undefined && appAuthToken === requestAuthToken) {
