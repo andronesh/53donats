01ae9f8dee047f23314a14aa5d6d9a02bc0990c8
Andrii Shalankevych <shalankevych@gmail.com>
2024-11-17 18:43:11 +0200

    Add protection for importing donats

--- src/app/api/donats/import/route.ts
+++ src/app/api/donats/import/route.ts
@@ -3,14 +3,26 @@ import { saveDonats } from "@/app/service/donatsService";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-	let donats = await getDonatsFromJar();
+	const requestHeaders = new Headers(request.headers);
+	const appAuthToken = process.env.APP_AUTH_TOKEN;
+	const requestAuthToken = requestHeaders.get("X-App-Auth-Token");
+	if (appAuthToken !== undefined && appAuthToken === requestAuthToken) {
+		let donats = await getDonatsFromJar();
 
-	const savedDonats = await saveDonats(donats);
-	console.info(`Imported ${saveDonats.length} donat${saveDonats.length === 1 ? "" : "s"}`);
-	return new Response(JSON.stringify(savedDonats), {
-		status: 200,
-		headers: {
-			"Content-Type": "application/json",
-		},
-	});
+		const savedDonats = await saveDonats(donats);
+		console.info(`Imported ${saveDonats.length} donat${saveDonats.length === 1 ? "" : "s"}`);
+		return new Response(JSON.stringify(savedDonats), {
+			status: 200,
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+	} else {
+		return new Response(JSON.stringify({ message: "WHAT ARE YOU TRYING TO DO?!" }), {
+			status: 403,
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+	}
 }
