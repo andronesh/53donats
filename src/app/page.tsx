9eb852cd67aaafe98b4edac09c61c4d1ba21fccb
Andrii Shalankevych <shalankevych@gmail.com>
2024-11-22 07:28:31 +0200

    Add loading spinner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { LoadingSpinner } from "@/components/common/LoadingSpinner";
 import DonatDetailsCompact from "@/components/donats/DonatDetailsCompact";
 import { getMainJar } from "@/db/dao/jarsDAO";
 import { DonatEntity, JarEntity } from "@/db/schema";
@@ -23,10 +24,14 @@ export default function Home() {
 		<div className="items-center justify-items-center min-h-screen p-4 pb-12 font-[family-name:var(--font-geist-sans)]">
 			<div className="flex flex-col items-center max-w-md pb-4">
 				<div className="flex flex-row justify-between w-full">
-					<h3 className={"text-2xl"}>
+					<h3 className={"text-2xl flex flex-row w-full"}>
 						<span>Зібрано </span>
-						<span className="font-bold">
-							{jar?.balance.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ")} грн.
+						<span className="font-bold w-full pl-2">
+							{jar ? (
+								jar.balance.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") + " грн."
+							) : (
+								<LoadingSpinner className="pl-4 max-h-8" />
+							)}
 						</span>
 					</h3>
 					<a
@@ -40,7 +45,9 @@ export default function Home() {
 				<h3 className={"text-xl"}>на мавіки для підрозділу БпЛА 53го ОСБ</h3>
 			</div>
 			<main className="flex flex-col items-center max-w-md">
-				{donats && donats.map((donat: DonatEntity) => <DonatDetailsCompact key={donat.id} donat={donat} />)}
+				{donats.length > 0 &&
+					donats.map((donat: DonatEntity) => <DonatDetailsCompact key={donat.id} donat={donat} />)}
+				{donats.length === 0 && <LoadingSpinner className="w-full max-h-20" />}
 			</main>
 		</div>
 	);
